0037e89b97a40903a720976cf0303f448385f44e
kadadot216 <yoctopolochoon54@gmail.com>
2018-04-10 19:00:08 +0200

    Now the duck texture is flipped when going back

--- src/game.c
+++ src/game.c
@@ -31,18 +31,28 @@ void	go_back(sfVector2f *speed)
 	speed->x = (- speed->x);
 }
 
+void	flip_sprite_h(sfSprite *sprite, int direction)
+{
+	sfVector2f	mirror_h = {direction, 1};
+	//sfVector2f	origin = {sfSprite_getLocalBounds(sprite).width, 0};
+
+	//sfSprite_setOrigin(sprite, origin);
+	sfSprite_setScale(sprite, mirror_h);
+}
+
 void	move_duck(t_duck *duck, t_window *window)
 {
 	float		maxwidth = window->mode.width - DUCK_WIDTH;
 	float		maxheight = window->mode.height - DUCK_HEIGHT;
 
 	duck->position = get_sp_position(duck->sprite);
-	if (duck->position.x < 0)
+	if (duck->position.x < 0) {
 		go_back(&duck->speed);
+		flip_sprite_h(duck->sprite, 1);
+	}
 	if (duck->position.x > maxwidth) {
 		go_back(&duck->speed);
-		my_put_nbr(duck->speed.x);
-		my_putstr("\n");
+		flip_sprite_h(duck->sprite, -1);
 	}
 	if  (duck->position.y == maxheight)
 		duck->speed.y = (- duck->speed.y);
